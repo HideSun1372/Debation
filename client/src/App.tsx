326d80279744f9c58099420855796ab88eccfc3e
Nandery <siqichen0802@gmail.com>
2026-03-21 22:28:33 -0700

    vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import { Analytics } from "@vercel/analytics/react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -84,6 +85,7 @@ function App() {
           </TooltipProvider>
         </UserProvider>
       </ThemeProvider>
+      <Analytics />
     </QueryClientProvider>
   );
 }
